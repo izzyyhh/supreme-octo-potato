c1740bf1e8ca6f6985ff1bcfe065ee3476f627b6
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2022-06-15 19:34:49 +0200

    add complex big-data

--- db/dump/modifier.js
+++ db/dump/modifier.js
@@ -5,15 +5,19 @@ const fileContent = fs.readFileSync(fileName).toString()
 const jsonArray = JSON.parse(fileContent)
 
 jsonArray.forEach((tweet) => {
-    tweet.author = (Math.random() + 1).toString(36).substring(7) // string
-    tweet.geo = (Math.random() + 1).toString(36).substring(7) // string
-    tweet.created = new Date(
-        +new Date() - Math.floor(Math.random() * 99999999999)
-    ) // string
-    tweet.likes = Math.floor(Math.random() * 1000) // number
-    tweet.retweets = Math.floor(Math.random() * 1000) // number
-    tweet.hashtags = ['league', 'izzy', 'bac'] // array of strings
-    tweet.status = 'published' // string
+    tweet.id = { id: tweet.id }
+    tweet.text = { text: tweet.text }
+    tweet.author = { author: (Math.random() + 1).toString(36).substring(7) } // string
+    tweet.geo = { geo: (Math.random() + 1).toString(36).substring(7) } // string
+    tweet.created = {
+        created: new Date(
+            +new Date() - Math.floor(Math.random() * 99999999999)
+        ),
+    } // string
+    tweet.likes = { likes: Math.floor(Math.random() * 1000) } // number
+    tweet.retweets = { retweets: Math.floor(Math.random() * 1000) } // number
+    tweet.hashtags = { hashtags: ['league', 'izzy', 'bac'] } // array of strings
+    tweet.status = { status: 'published' } // string
 })
 
 fs.writeFileSync(fileName, JSON.stringify(jsonArray))
